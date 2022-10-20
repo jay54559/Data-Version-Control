607b1eab21eafd832d0eb7a87f7226ddd00fc36a
Jay Patel <jaypatel.arn@gmail.com>
2022-10-20 22:42:18 +0200

    ADD the module data_load

---
+++ course_ds_base/src/stages/data_load.py
@@ -0,0 +1,32 @@
+"""
+This is the module for data loading
+Stage 1 in pipeline
+"""
+
+from typing import Text
+import pandas as pd
+from sklearn.datasets import load_iris
+import yaml
+import argparse
+
+def data_load(config_path: Text) -> None:
+    """ Load Raw Data
+    Args:
+        config_path {Text}: Path to config file
+    """
+    with open("params.yaml") as config_path:
+        config = yaml.safe_load(config_path)
+
+    data = load_iris(as_frame=True)
+    dataset = data.frame
+    dataset.columns = [colname.strip(' (cm)').replace(' ', '_') for colname in dataset.columns.tolist()]
+    dataset.to_csv(config["data"]["dataset_csv"], index=False)
+
+    print("Data Loading Done. \n")
+
+if __name__ == "__main__":
+    args_parser = argparse.ArgumentParser(description="Process the config file")
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+
+    data_load(config_path=args.config)
